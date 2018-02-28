a7abdabed91d06e82c8a871bf678f4f25648befc
DAEMonRaco <daemonraco@gmail.com>
2018-02-27 23:30:37 -0300

    More Commands
    * Adding a command to remove all data of a collection.
    * Bug fixes.

---
+++ commands/truncate.cmd.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const chalk = require('chalk');
+const { DocsOnFileDB } = require('dfdb');
+const Promise = require('es6-promise');
+
+const { CURRENT_DB } = require('../includes/constants');
+
+module.exports = {
+    commands: ['truncate'],
+    description: 'Removes all data of certain collection.',
+    usage: 'truncate collection-name',
+    completer: 'collection',
+    runner: ({ manager, args }) => {
+        return new Promise((resolve, reject) => {
+            const conn = manager.get(CURRENT_DB);
+            const collectionName = args.shift();
+            let error = false;
+
+            if (!collectionName) {
+                error = `No collection name given.`;
+            }
+            if (!conn) {
+                error = `Not connected to any database`;
+            }
+
+            if (!error) {
+                conn.collection(collectionName)
+                    .then(col => {
+                        col.truncate().then(() => {
+                            resolve(chalk.green(`Collection truncated`));
+                        }).catch(err => reject(`${err}`));
+                    })
+                    .catch(err => reject(`${err}`));
+            } else {
+                reject(error);
+            }
+        });
+    }
+}
